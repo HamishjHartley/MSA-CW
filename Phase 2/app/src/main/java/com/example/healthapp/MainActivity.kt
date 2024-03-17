8eb9606311e4c6054683b93a3e19c22582851863
“HamishjHartley” <113518445+HamishjHartley@users.noreply.github.com>
2024-03-17 12:40:45 +0000

    started step counter

--- Phase 2/app/src/main/java/com/example/healthapp/MainActivity.kt
+++ Phase 2/app/src/main/java/com/example/healthapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.healthapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,20 +11,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
 import com.example.healthapp.ui.theme.HealthAppTheme
+import com.example.healthapp.ui.theme.HomeViewModel
 
 class MainActivity : ComponentActivity() {
     //Instance variables, TODO: Move to container
-    private lateinit var stepCounter: StepCounter
-
-
+    private lateinit var viewModel: HomeViewModel
+    private lateinit var stepCountTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        stepCounter = StepCounter(this, this)
-        stepCounter.register()
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        viewModel.stepCount.observe(this, { stepCount ->
+            stepCountTextView.text = "Step Count: $stepCount"
+        })
         setContent {
             HealthAppTheme {
                 // A surface container using the 'background' color from the theme
